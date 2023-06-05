f0c7114e0f9222783c28f2d57f8d4f55b8d790c8
michaelfmnk <michael.fmnk@gmail.com>
2023-06-06 00:49:35 +0300

    added Polish support

--- app/src/main/java/dev/fomenko/latinhelper/data/Phrase.kt
+++ app/src/main/java/dev/fomenko/latinhelper/data/Phrase.kt
@@ -11,34 +11,38 @@ data class Phrase(
     val en: String = "",
     val uk: String = "",
     var isFavorite: Boolean = false,
+    val pl: String = "",
 ) {
 
     fun translation(lang: Locale): String {
         return when (lang.language) {
             "en" -> en
             "uk" -> uk
+            "pl" -> pl
             else -> en
         }
     }
 }
 
 enum class PhraseSort(
+    val lang: String,
     createComparator: () -> Comparator<Phrase>,
 ) {
-    Latin({ compareBy { it.phrase } }),
-    English({ compareBy { it.en } }),
-    Ukrainian({
+    Latin("latin", { compareBy { it.phrase } }),
+    English("en", { compareBy { it.en } }),
+    Ukrainian("uk", {
         val collator = Collator.getInstance(java.util.Locale("uk", "UA"))
         compareBy { collator.getCollationKey(it.uk) }
+    }),
+    Polish("pl", {
+        val collator = Collator.getInstance(java.util.Locale("pl", "PL"))
+        compareBy { collator.getCollationKey(it.pl) }
     });
 
     val comparator: Comparator<Phrase> = createComparator()
 
     companion object {
-        fun valueOf(value: Locale): PhraseSort = when (value.language) {
-            "en" -> English
-            "uk" -> Ukrainian
-            else -> Latin
-        }
+        fun valueOf(value: Locale): PhraseSort = PhraseSort.values()
+            .find { it.lang == value.language } ?: Latin
     }
 }
--- app/src/main/java/dev/fomenko/latinhelper/presentation/PhraseListItem.kt
+++ app/src/main/java/dev/fomenko/latinhelper/presentation/PhraseListItem.kt
@@ -1,6 +1,5 @@
 package dev.fomenko.latinhelper.presentation
 
-import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
